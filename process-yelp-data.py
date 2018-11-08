d6b002edb425adfeb80b8cb2b0821719f9c7e728
Taya Kitt <taya.kitt@gmail.com>
2018-11-08 13:11:15 -0500

    filter restaurants in toronto

--- process-yelp-data.py
+++ process-yelp-data.py
@@ -2,5 +2,11 @@ import json
 import pandas as pd
 
 df = pd.read_pickle("data.pkl")
+print(list(df.columns.values))
 
-print(df)
+# get all restaurants in toronto that is open
+df_toronto = df.loc[(df['city'].isin(['Toronto'])) & (df['is_open']==True)]
+
+
+print (df_toronto.neighborhood.unique().tolist())
+print (df_toronto.shape)
