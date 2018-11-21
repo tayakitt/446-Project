023aab72c222704ca94dc5c982929c1e44f40659
Tyler Lam <tylerlam888@gmail.com>
2018-11-21 14:26:28 -0500

    joined ward and neighborhood data

--- join_data.py
+++ join_data.py
@@ -22,9 +22,16 @@ with open('yelp_mapping.csv', mode='r') as csv_file:
     for row in csv_reader:
         yelp_mapping[row[0]] = row[1]
 
+hood_to_ward = {}
+with open('hood-to-ward.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        hood_to_ward[row[0]] = row[1]
+
 print(yelp_mapping)
 print(postals)
 print(nbh_names)
+print(hood_to_ward)
 
 def contains_hood(hood_name, all_names):
     for name in all_names:
@@ -51,4 +58,31 @@ def map_yelp_to_toronto(yelp_name):
     if yelp_name in yelp_mapping:
         return yelp_mapping[yelp_name]
     return False
+
+hood_data = pd.read_csv("toronto-data.csv").transpose()
+hood_data.columns = hood_data.iloc[0]
+hood_data.reindex(hood_data.index.drop("Unnamed: 0"))
+hood_data["nbhd"] = hood_data.index.values
+
+ward_data = pd.read_csv("ward-data.csv").transpose()
+ward_data.columns = ward_data.iloc[0]
+ward_data.reindex(ward_data.index.drop("Unnamed: 0"))
+ward_data["Ward"] = ward_data.index.values
+print(ward_data)
+
+# assign wards to neighbourhoods
+ward_column = []
+for index, row in hood_data.iterrows():
+    if index in hood_to_ward:
+        ward_column.append("Ward " + hood_to_ward[index])
+    else:
+        ward_column.append(None)
+
+hood_data["Ward"] = ward_column
+print(ward_column)
+
+hoods_and_wards = pd.merge(hood_data, ward_data, on="Ward", how="outer")
+hoods_and_wards.index = hoods_and_wards["nbhd"]
+
+print(hoods_and_wards)
 
